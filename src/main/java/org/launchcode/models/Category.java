5d3b17dec248c9497e6400453803166d6f960548
Nora Harris <noragharris@gmail.com>
2017-12-21 10:18:51 -0600

    work on cheese mvc persistance assignment, right before deleted cheesetype enum

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,51 @@
+package org.launchcode.models;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+//    declare fields
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+//    getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+//    default constructor
+    public Category() {}
+
+//regular constructor
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+
+
+}
